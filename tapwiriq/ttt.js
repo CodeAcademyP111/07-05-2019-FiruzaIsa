2a8843913d48e2eb734c5b2f4d923f5303c68b09
FiruzaIsa <49427451+FiruzaIsa@users.noreply.github.com>
2019-05-07 16:53:41 +0400

    Add files via upload

---
+++ tapwiriq/ttt.js
@@ -0,0 +1,28 @@
+// var say=Number(prompt("reqem daxil et"));
+// var soz=prompt("soz daxil et");
+
+// function thenumberofwords (num,word){
+//     for(var i=0; i<num; i++){
+//         console.log(word);
+//     }
+
+// }
+// thenumberofwords(say,soz);
+
+// var eded = Number(prompt("eded goster"))
+
+
+// function findcount (num,count){
+//     for (var i=0; i<count; i++){
+
+//     }
+// }
+ 
+
+
+var reqem=Number (prompt("reqem daxil et"));
+function mertebe (num) {
+   var i= num.toString().length;
+    console.log(i);
+}
+mertebe(reqem);
